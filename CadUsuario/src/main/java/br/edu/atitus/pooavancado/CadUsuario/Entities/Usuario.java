fba9457aada42e4935c844ae580fee443dab66c9
Luciano Rodrigo Ferretto <prof.ferretto@yahoo.com.br>
2023-03-22 22:02:01 -0300

    Adição das dependências Spring Data JPA e PostgreSQL Driver
    Configuração da Conexão com o Banco de dados
    Configuração da Entidade Usuário

--- CadUsuario/src/main/java/br/edu/atitus/pooavancado/CadUsuario/Entities/Usuario.java
+++ CadUsuario/src/main/java/br/edu/atitus/pooavancado/CadUsuario/Entities/Usuario.java
@@ -1,11 +1,33 @@
 package br.edu.atitus.pooavancado.CadUsuario.Entities;
 
-public class Usuario {
-	
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(nullable = false, length = 150)
 	private String nome;
+	
+	@Column(nullable = true, length = 200)
 	private String email;
+	
+	@Column(nullable = false)
 	private boolean status;
+	
 	public long getId() {
 		return id;
 	}
